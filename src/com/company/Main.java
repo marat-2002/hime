130790e3810e3340f80d98cbbb12d850e2e9b2dd
MArat <95515610+marat-2002@users.noreply.github.com>
2021-12-04 12:30:16 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите ваше имя:");
+        String name = in.nextLine();
+        System.out.println("Здраствуйте," + in);
+
+
+    }
+}
